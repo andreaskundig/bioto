bbb8699fadfa5b017e85a6b535c40c6ddcd115e2
ak <ak@ak.ak>
2011-07-03 14:37:03 +0200

    morpher eliminated

--- server/test.js
+++ server/test.js
@@ -40,38 +40,6 @@ test("build_paragraph",function() {
 
  });
 
-test("Morpher.overlap", function(){
-    equal(Morpher.overlap("Loana","Bobo",0,true),"Loana");
-    equal(Morpher.overlap("Loana","Bobo",1,true),"LoanB");
-    equal(Morpher.overlap("Loana","Bobo",2,true),"LoaBo");
-    equal(Morpher.overlap("Loana","Bobo",3,true),"LoBob");
-    equal(Morpher.overlap("Loana","Bobo",4,true),"LBobo");
-    equal(Morpher.overlap("Loana","Bobo",5,true),"Bobo");
-
-    equal(Morpher.overlap("Lo","Bobo",0,true),"Lo");
-    equal(Morpher.overlap("Lo","Bobo",1,true),"LB");
-    equal(Morpher.overlap("Lo","Bobo",2,true),"Bo");
-    equal(Morpher.overlap("Lo","Bobo",3,true),"Bob");
-    equal(Morpher.overlap("Lo","Bobo",4,true),"Bobo");
-
-    equal(Morpher.overlap("Fred","centipede", 0,true), "Fred");
-    equal(Morpher.overlap("Fred","centipede", 1,true), "Frec");
-    equal(Morpher.overlap("Fred","centipede", 2,true), "Frce");
-    equal(Morpher.overlap("Fred","centipede", 3,true), "Fcen");
-    equal(Morpher.overlap("Fred","centipede", 4,true), "cent");
-    equal(Morpher.overlap("Fred","centipede", 5,true), "centi");
-    equal(Morpher.overlap("Fred","centipede", 6,true), "centip");
-    equal(Morpher.overlap("Fred","centipede", 7,true), "centipe");
-    equal(Morpher.overlap("Fred","centipede", 8,true), "centiped");
-    equal(Morpher.overlap("Fred","centipede", 9,true), "centipede");
-
-    equal(Morpher.overlap("Loana","Bobo",0,false),"Bobo");
-    equal(Morpher.overlap("Loana","Bobo",1,false),"LBob");
-    equal(Morpher.overlap("Loana","Bobo",2,false),"LoBo");
-    equal(Morpher.overlap("Loana","Bobo",3,false),"LoaB");
-    equal(Morpher.overlap("Loana","Bobo",4,false),"Loan");
-    equal(Morpher.overlap("Loana","Bobo",5,false),"Loana");
- });
 
 test("safe_index", function(){
   var a = [1,2,3];
@@ -83,17 +51,6 @@ test("safe_index", function(){
   equal(a[array_util.safe_index(a.length, 3)], 3);
 
  });
-function animate(morpher){
-  var display = function(i){ return function(){
-     $('#show').html(morpher.step(i));}; };
-  for(var i=0; i<morpher.nb_steps; i+=1 ){
-      setTimeout(display(i), i*50);
-  };
-  for(var j=0; j<morpher.nb_steps; j+=1 ){
-      setTimeout(display(morpher.nb_steps-j-1), (morpher.nb_steps+j)*50+1000);
-  };
-
- }
 
 test("make_sub", function(){
     var text, ph, replacement, sub;
@@ -111,51 +68,6 @@ test("make_sub", function(){
     deepEqual(substituter.make_sub(ph,replacement),["#1#","Asphalte","Fred"]);
 
  });
-test("morpher", function(){
-    var  text, morpher, stages, i, subs;
-    text = {text: "En 2001, #0# vient chez #1#.",
-	    placeholders:[["#0#", null, "Loana", "nom_feminin_1"],
-                          ["#1#", null, "Fred", "prenom_masculin_2"]] };
-    morpher = new Morpher(text,["Bozo","centipede"]);
-    subs = morpher.sub_holder.subs;
-    deepEqual(subs[0],["#0#","Bozo","Loana"]);
-    deepEqual(subs[1],["#1#","centipede","Fred"]);
-
-    stages = Morpher.morph_stages(subs[0],true);
-    deepEqual(stages, ["Loana","LoanB","LoaBo","LoBoz","LBozo","Bozo"]);
-    
-    equal(morpher.morphs.length,2);
-    equal(morpher.morphs[0].length,6);
-    deepEqual(morpher.morphs[0], ["Bozo", "LBoz", "LoBo", "LoaB", "Loan", "Loana"]);
-    equal(morpher.morphs[1].length,10);
-
-    equal(morpher.nb_steps,15);
-
-    equal(morpher.offsets[0],0);
-    equal(morpher.offsets[1],5);
-    equal(morpher.offsets[2],14);
-    
-    deepEqual(morpher.replacements_for_step(0),["Bozo", "centipede"]);
-    deepEqual(morpher.replacements_for_step(1),["LBoz", "centipede"]);
-    deepEqual(morpher.replacements_for_step(5),["Loana", "centipede"]);
-    deepEqual(morpher.replacements_for_step(6),["Loana", "Fcentiped"]);
-    deepEqual(morpher.replacements_for_step(7)["Bozo","Fredce"]);
-    deepEqual(morpher.replacements_for_step(8)["Bozo","Fredcen"]);
-    deepEqual(morpher.replacements_for_step(9)["Bozo","Fredcent"]);
-    deepEqual(morpher.replacements_for_step(10)["Bozo","Fredcenti"]);
-    deepEqual(morpher.replacements_for_step(11)["Bozo","Frecentip"]);
-    deepEqual(morpher.replacements_for_step(12)["Bozo","Frcentipe"]);
-    deepEqual(morpher.replacements_for_step(13)["Bozo","Fcentiped"]);
-    deepEqual(morpher.replacements_for_step(14)["Bozo","centipede"]);
-
-    equal(morpher.step(2),'En 2001, LoBo vient chez centipede.');
-    equal(morpher.step(7),'En 2001, Loana vient chez Frcentipe.');
-    equal(morpher.step(13),'En 2001, Loana vient chez Fredc.');
-
-    animate(morpher);
-
- });
- 
 test("span_words",function(){
    var text = span_displayer.span_words(
    "En 01, <span class='o'>Loa na</span> vient sf<span> ah ah </span>bof. <br>hop");
